ab1b62c06e6a2673fa4051e49c1bb34424ae5c59
nandocembra <45723377+nandocembra@users.noreply.github.com>
2020-02-17 20:32:23 -0300

    Criado projeto Alien City

---
+++ Alien City/Assets/Scripts/GunScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public Animator anim;                               // Referência para o Animator do objeto Tiro
+    public Rigidbody2D rigidBody;                       // Referência para o Rigidbody2D do objeto Tiro
+    public float fire1Speed;                            // A velocidade que o projétil se move
+    public float timeToDestroy = 5f;                    // O tempo que leva para o projétil se destruido, limitando seu alcance
+    public bool facingRight;                            // Para qual lado o projétil será disparado
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        fire1Speed = 20f;
+        timeToDestroy = 5f;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Define quanto o projétil se moverá na tela
+
+        // Move o projétil no eixo horizontal
+        if (facingRight)
+        {
+//            rigidBody.velocity = new Vector2 (fire1Speed, rigidBody.velocity.y);
+            rigidBody.AddForce(transform.right * fire1Speed);
+        }
+        else
+        {
+//            rigidBody.velocity = new Vector2(-fire1Speed, rigidBody.velocity.y);
+            rigidBody.AddForce(transform.right * -fire1Speed);
+        }
+
+        // Destói o projétil quando alcançar o limite de tempo
+        Destroy(this.gameObject, timeToDestroy);
+    }
+
+
+
+
+    // Recebe mensagem do PlayerScript indicando para qual lado o projétil está sendo disparado
+    void FacingR()
+    {
+        facingRight = true;
+    }
+
+    void FacingL()
+    {
+        facingRight = false;
+        Flip();
+    }
+
+    // Multiplica o componente x do localScale por -1, fazendo o sprite do projétil virar para o outro lado
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    // Detecta colisão do tiro
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string otherTag = collision.gameObject.tag;
+
+        // Compara a Tag do objeto envolvido na colisão e destrói o projétil
+        if (otherTag == "Untagged")
+        {
+            anim.SetTrigger("Hit");           // carrega animação Hit do tiro (tiro_arma1_hit)
+            rigidBody.velocity = new Vector2 (-10, 0);
+            Destroy(this.gameObject, 0.06f);
+        }
+    }
+
+}
---
+++ Alien City/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	public Transform posPe;
+	[HideInInspector] public bool tocaChao = false;
+
+
+	public float Velocidade;
+	public float ForcaPulo = 1000f;
+	[HideInInspector] public bool viradoDireita = true;
+
+	public Image vida;
+	private MensagemControle MC;
+
+	public bool armaEquipada = false;					 // Variável booleana, verdadeiro se uma arma está equipada, falso se nenhuma arma está equipada
+	public GameObject tiro1Prefab;                       // Referência ao prefab do projétil que será disparado pelo Player com a arma 1 
+	private AudioSource audioSrc;                        // Usado para guardar uma referência para o componente Autio Source do Player
+	public AudioClip shootSound;                         // Clipe de Audio do som do disparo da arma
+
+	private float translationY;							// Recebe o movimento vertical do Player
+	private float translationX;							// Recebe o movimento horizontal do Player
+
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+		audioSrc = GetComponent<AudioSource>();
+		tocaChao = true;
+
+		GameObject mensagemControleObject = GameObject.FindWithTag ("MensagemControle");
+		if (mensagemControleObject != null) {
+			MC = mensagemControleObject.GetComponent<MensagemControle> ();
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Ao apertar Ctrl (Fire1), se estiver parado e tocando no chão, equipa arma
+		if (Input.GetButtonDown("Fire1"))
+		{
+			if (armaEquipada == false)
+			{
+				armaEquipada = true;
+			}
+			else
+			{
+				armaEquipada = false;
+			}
+		}
+
+		// Ao apertar Alt (Fire2), testa se uma arma está equipada, e instancia o tiro
+		if (Input.GetButtonDown("Fire2"))
+		{
+			if (armaEquipada == true)
+			{
+
+				// Guarda a posição do Player, posiciona na variável position e prepara para instanciar o projétil em frente a arma do Player
+				Vector3 position = new Vector3();
+				if (viradoDireita)
+				{
+					position = new Vector3(transform.position.x + ((transform.localScale.x / 2f) + 0.6f), transform.position.y - (transform.localScale.y - 1.1f));
+				}
+				else
+				{
+					position = new Vector3(transform.position.x + ((transform.localScale.x / 2f) - 0.6f), transform.position.y - (transform.localScale.y - 1.1f));
+				}
+
+				// Guarda uma referência ao prefab instanciado (projétil)
+				GameObject instance = Instantiate(tiro1Prefab, position, Quaternion.identity) as GameObject;
+
+				// Envia uma mensagem ao objeto instanciado (projétil) indicando a direção em que foi disparado
+				if (viradoDireita)
+				{
+					instance.gameObject.SendMessage("FacingR");
+				}
+				else
+				{
+					instance.gameObject.SendMessage("FacingL");
+				}
+
+				// Executa o som do tiro
+				audioSrc.clip = shootSound;
+				audioSrc.Play();
+
+
+
+				//Implementar Pulo Aqui!
+
+
+
+			}
+		}
+
+	}
+
+
+	void FixedUpdate()
+	{
+//	Alterei a declaração dessas variáveis, passando para variáveis globais da Classe para corrigir um bug nas animações do Player
+//		float translationY = 0;
+//		float translationX = Input.GetAxis ("Horizontal") * Velocidade;
+
+		translationY = 0;
+		translationX = Input.GetAxis("Horizontal") * Velocidade;
+		transform.Translate (translationX, translationY, 0);
+		transform.Rotate (0, 0, 0);
+
+		if (translationX != 0 && tocaChao) {
+			if (armaEquipada == false)
+			{
+				anim.SetTrigger("corre");	     // Se arma não está equipada chama animação corre (run)
+			} else
+			{
+				anim.SetTrigger("arma1_corre");		// Se arma está equipada chama Animação correndo com a arma (corre_arma1)
+			}
+		
+		} else {
+			if (translationX == 0 && tocaChao) {
+				if (armaEquipada == false)
+				{
+					anim.SetTrigger("parado");      // Se arma não está equipada chama animação parado (stand)
+				} else
+				{
+					anim.SetTrigger("arma1_parado");    // Se arma está equipada chama Animação parado com a arma (stand_shoot)
+				}
+			}
+		
+
+
+		}
+
+
+		//Programar o pulo Aqui! 
+
+
+		if (translationX > 0 && !viradoDireita) {
+			Flip ();
+		} else if (translationX < 0 && viradoDireita) {
+			Flip();
+		}
+
+
+	}
+	
+
+	void Flip()
+	{
+		viradoDireita = !viradoDireita;
+		Vector3 escala = transform.localScale;
+		escala.x *= -1;
+		transform.localScale = escala;
+	}
+
+	public void SubtraiVida()
+	{
+		vida.fillAmount-=0.1f;
+		if (vida.fillAmount <= 0) {
+			MC.GameOver();
+			Destroy(gameObject);
+		}
+	}
+	
+}
