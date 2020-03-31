79ed0eedc458445dc36fd6f2e2646eb9e484acbc
nandocembra <45723377+nandocembra@users.noreply.github.com>
2020-03-30 23:17:15 -0300

    Alien City 3D Fase 5
    Desenvolvimento da Fase 5  do Alien City 3D, com personagem e mecânicas funcionando.

---
+++ Alien City 3D/AlienCity3D_Fase5/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        anim.SetTrigger("Ativar");
+    }
+}
